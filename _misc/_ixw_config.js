1157e5145a9ca5246fbc9ec85711855a2771c7ba
Lance <lance_amethyst@hotmail.com>
2015-01-21 12:08:04 +0800

    grunt.deploy done

--- _misc/_ixw_config.js
+++ _misc/_ixw_config.js
@@ -89,11 +89,11 @@ module.exports = {
 		},
 		copy: {
 			deploy: {
-				files:[  
+				files: [
 				    //{src: ['path/*'], dest: 'dest/', filter: 'isFile'},// 复制path目录下的所有文件  
 				    //{src: ['path/**'], dest: 'dest/'},// 复制path目录下的所有目录和文件  
 					{src: ['src/bootstrap/fonts/*'], dest: 'dist/bootstrap/fonts/', expand: true, flatten: true, filter: 'isFile'},
-					{src: ['proto/dist/*'], dest: 'dist/', expand: true, flatten: true, filter: 'isFile'}	
+					{src: ['proto/dist/*'], dest: 'dist/', expand: true, flatten: true, filter: 'isFile'}
 				]
 			}
 		}
