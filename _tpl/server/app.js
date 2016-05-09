f0a2cecce6bd7cf13c45731e25b821f39cbb059e
Lance <lance_amethyst@hotmail.com>
2016-05-09 15:00:38 +0800

    remove jade

--- _tpl/server/app.js
+++ _tpl/server/app.js
@@ -12,7 +12,7 @@ var app = express();
 var cookieSecret = pkgName + "-project";
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'html');
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
