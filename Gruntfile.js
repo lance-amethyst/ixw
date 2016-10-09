e748b3c9ada292b3b29f019339cdbe17f9e394c8
Lance <lance_amethyst@hotmail.com>
2016-10-09 17:35:32 +0800

    apply grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,9 +4,8 @@ require("./bin/_prompt/index.js");
 var path =  require('path');
 var gruntConfig = require("./grunt-config.js");
 
-module.exports = function (grunt) {
+function chkconfig(){
 	var prjCfg = null;
-
 	try {
 		prjCfg = require('./ixw-config.js');
 	}catch(e){}
@@ -16,15 +15,36 @@ module.exports = function (grunt) {
 		console.log("Also you can create it manually with template file: config-template.js.");
 		process.exit();
 	}
+	if (!prjCfg.name){
+		console.log("Wrong in file ixw-config.js : name is undefined!");
+		console.log("You can run `node config` to create it automatically.");
+		console.log("Also you can create it manually with template file: config-template.js.");
+		process.exit();
+	}
+	var prjType = $XP(prjCfg, "type");
+	if (prjType!="pure" && prjType != "mixed")
+		prjCfg.type = "pure";
+	var prjNS = $XP(prjCfg, "ns");
+	if (IX.isEmpty(prjNS))
+		prjNS.ns = prjCfg.name.toUpperCase();
+	var prjPath = $XP(prjCfg, "path");
+	if (IX.isEmpty(prjPath))
+		prjNS.path = "../" + prjCfg.name;
 
-	grunt.initConfig(IX.inherit(gruntConfig, {config : prjCfg}));
+	prjCfg.dir = path.normalize(prjCfg.path);
+
+	return IX.inherit(gruntConfig, {config : prjCfg});
+}
+
+module.exports = function (grunt) {
+	var gruntCfg = chkconfig();
+
+	grunt.initConfig(gruntCfg);
 	grunt.loadNpmTasks("grunt-contrib-jshint");
 	grunt.loadNpmTasks("grunt-contrib-concat");
 	grunt.loadNpmTasks("grunt-contrib-copy");
 	grunt.loadNpmTasks("grunt-string-replace");
 	grunt.loadNpmTasks("grunt-contrib-clean");
-
-	prjCfg.dir = path.normalize(prjCfg.path);
 
 	var taskEntry = require("./bin/project/index.js");
 	// register task : "compileETS" and "cleanCompiledETS",
